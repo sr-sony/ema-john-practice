00d4c8ed2a7b89460be7bf444018bb40d8a9999f
sr-sony <sr.sony.05@gmail.com>
2022-10-07 21:26:11 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import About from './components/About/About';
+import Main from './Layout/Main';
+import Shop from './components/Shop/Shop'
+import Orders from './components/Orders/Orders';
+import Inventory from './components/Inventory/Inventory';
+import { productsAndCartLoader } from './loaders/productsAndCartLoader';
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children:[
+        {
+          path:'/shop',
+          loader: () => fetch('products.json'),
+          element: <Shop></Shop>
+        },
+        {
+          path: '/orders',
+          loader: productsAndCartLoader,
+          element: <Orders></Orders>
+        },
+        {
+          path: '/inventory',
+          element: <Inventory></Inventory>
+        },
+        {
+          path:"about",
+          element: <About></About>
+        }
+      ]
+    }
+    
+  ])
+  return (
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/loaders/productsAndCartLoader.js
@@ -0,0 +1,22 @@
+import { getStoredCart } from "../utilities/fakedb";
+
+export const productsAndCartLoader = async () => {
+    const productsData = await fetch('products.json');
+    const products = await productsData.json();
+
+
+    //get cart
+    const savedCart = getStoredCart();
+    const previousCart = [];
+    for(const id in savedCart){
+        const addedProduct = products.find(product => product.id === id);
+        if(addedProduct){
+            const quantity = savedCart[id];
+            addedProduct.quantity = quantity;
+            previousCart.push(addedProduct);
+
+        }
+    }
+
+    return {products, previousCart};
+}
